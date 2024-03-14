3c60d5f321f0b13b1bbf1b6f106d6c7c8ede97e6
ILykSonic <147686259+ILykSonic@users.noreply.github.com>
2024-03-14 12:44:32 -0500

    Update wstation.cpp

--- wstation.cpp
+++ wstation.cpp
@@ -1,51 +1,87 @@
 #include <fstream>
-#include <string>
 #include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <string>
 
 using namespace std;
 
-int main() {
-    ifstream inFile(R"(C:\Users\ILykS\CLionProjects\zCS121 HW3\AL_Weather_Stations_Dec_23 (1).txt)");
-    ofstream outFile("output.txt");
-    string line;
-    bool isHeader = true;
-
-    if (inFile.is_open()) {
-        while (getline(inFile, line)) {
-            if (isHeader) {
-                // Process the header line
-                string header1 = line.substr(18, 23);
-                // all of these skip create my header and skip the first header (Station) and starts with station_name
-                string header2 = line.substr(65, 46);
-                string header3 = line.substr(128, 6);
-                string header4 = line.substr(164, 18);
-                string newheader = header1 + header2 + header3 + header4;
-                outFile << newheader << '\n';
-                isHeader = false;
-            } else {
-                // Process the content lines
-                string part1 = line.substr(18, 50);  //Station_Name
-                part1.erase(0, part1.find_first_not_of(" "));  // Remove leading spaces
-                string part2 = line.substr(70, 11); // Elevation
-                string part3 = line.substr(82, 20); // Latitude and Longitude
-                string part4 = line.substr(102, 9);  // Date
-                string part5 = line.substr(128, 7);  // PRCP
-                string part6 = line.substr(163, 16);  // TMAX/TMIN
-                string newLine = part1 + part2 + part3 + part4 + part5 + part6;
-
-                size_t pos;
-                string token = "-9999";
-                while ((pos = newLine.find(token)) != string::npos) {
-                    newLine.erase(pos, token.length());
-                }
-                outFile << newLine << '\n';
-            }
-        }
-        inFile.close();
-        outFile.close();
-    } else {
-        cout << "Unable to open file";
+bool isFloat(string s) {
+    if (s == "-9999") {
+        return false;
     }
+    try {
+        stof(s);
+        return true;
+    } catch (const std::exception& e) {
+        return false;
+    }
+}
+
+int main() {
+    ifstream infile(R"(C:\Users\nilan\CLionProjects\untitled\cmake-build-debug\AL_Weather_Stations_Dec_23.txt)");
+    ofstream outfile("output.txt");
+
+    if (!infile || !outfile) {
+        cerr << "Error opening files!" << endl;
+        return 1;
+    }
+
+    string line;
+    while (getline(infile, line)) {
+        istringstream iss(line);
+        string station, elevation, latitude, longitude, date, prcp, tmax, tmin;
+
+        iss >> ws;
+        getline(iss, station, ',');
+        iss >> ws;
+        getline(iss, elevation, ',');
+        iss >> ws;
+        getline(iss, latitude, ',');
+        iss >> ws;
+        getline(iss, longitude, ',');
+        iss >> ws;
+        getline(iss, date, ',');
+        iss >> ws;
+        getline(iss, prcp, ',');
+        iss >> ws;
+        getline(iss, tmax, ',');
+        iss >> ws;
+        getline(iss, tmin, ',');
+
+        // Reformatting data
+        if (date.length() >= 8) {
+            date = date.substr(4, 2) + " " + date.substr(6, 2) + " " + date.substr(0, 4);
+        }
+
+        station.erase(0, station.find_first_not_of(' ')); // Remove leading spaces
+
+        // Convert strings to floating-point numbers
+        float elevation_f, latitude_f, longitude_f, prcp_f, tmax_f, tmin_f;
+        if (isFloat(elevation) && isFloat(latitude) && isFloat(longitude) &&
+            isFloat(prcp) && isFloat(tmax) && isFloat(tmin)) {
+            elevation_f = stof(elevation);
+            latitude_f = stof(latitude);
+            longitude_f = stof(longitude);
+            prcp_f = stof(prcp);
+            tmax_f = stof(tmax);
+            tmin_f = stof(tmin);
+
+            // Write to the new file with formatted data
+            outfile << line << endl;  // Move this line here
+            outfile << left << setw(25) << station << fixed << setprecision(2) << setw(10) << elevation_f
+                    << setprecision(4) << setw(10) << latitude_f << setw(10) << longitude_f << " " << date << " "
+                    << prcp_f << " " << tmax_f << " " << tmin_f << endl;
+        } else {
+            // Skip the current line if any conversion fails
+            continue;
+        }
+    }
+    infile.close();
+    outfile.close();
+    return 0;
+}
+
 
     return 0;
 }
